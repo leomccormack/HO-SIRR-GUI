79ac012b14c9d78730033117b7739adaf5ddc602
leomccormack <leo.t.mccormack@gmail.com>
2020-12-16 12:07:41 +0200

    added tooltips to describe the app

--- audio_plugin/_HOSIRR_/src/PluginEditor.h
+++ audio_plugin/_HOSIRR_/src/PluginEditor.h
@@ -130,6 +130,7 @@ private:
 
     /* tooltips */
     SharedResourcePointer<TooltipWindow> tipWindow;
+    std::unique_ptr<juce::ComboBox> pluginDescription; /* Dummy combo box to provide plugin description tooltip */
 
     //[/UserVariables]
 
